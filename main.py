1aa19a6383e5924d420f413878658a8112202613
Niclas <njdm.evil@gmail.com>
2017-05-31 21:07:11 +0200

    Better flow

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import client_data
 
 import praw
 import re
+import time
 
 print("GrammazNaziBot2k loading...")
 
@@ -33,30 +34,34 @@ reddit = praw.Reddit(
 )
 
 try:
-    for comment in reddit.subreddit('all').stream.comments():
-        if re.search(needs_fix_he_she, comment.body):
-            comment.reply("""You may use the *gender-neutral*, *singular* `they` when talking about a person with unknown gender.
+    while 1:
+        try:
+            for comment in reddit.subreddit('all').stream.comments():
+                if re.search(needs_fix_he_she, comment.body):
+                    comment.reply("""You may use the *gender-neutral*, *singular* `they` when talking about a person with unknown gender.
 Click [this](https://en.wikipedia.org/wiki/Singular_they) for more info.
 I've corrected {} people before you.
 
 ^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.
 ^If ^there's ^something ^wrong ^please ^message ^SteveCCL.""".format(corrected))
-            corrected += 1
-            print("Corrected {}".format(comment))
-        elif re.search(needs_fix_his_hers, comment.body):
-            comment.reply("""You may use the *gender-neutral*, *singular* `their` when talking about a person with unknown gender.
+                    corrected += 1
+                    print("Corrected {}".format(comment))
+                elif re.search(needs_fix_his_hers, comment.body):
+                    comment.reply("""You may use the *gender-neutral*, *singular* `their` when talking about a person with unknown gender.
 Click [this](https://en.wikipedia.org/wiki/Singular_they) for more info.
 I've corrected {} people before you.
 
 ^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.
 ^If ^there's ^something ^wrong ^please ^message ^SteveCCL.""".format(corrected))
-            corrected += 1
-            print("Corrected {}".format(comment))
+                    corrected += 1
+                    print("Corrected {}".format(comment))
+
+        except:
+            print("Something went wrong")
+            time.sleep(60)
+            print("Resuming")
 except KeyboardInterrupt:
     print("User requested termination")
-
-except:
-    print("Something went wrong")
 
 with open("db.txt", 'w') as f:
     f.write(str(corrected))
