5d8a4ab11de46b61e26b619c02ca8771a49c27b7
Niclas <njdm.evil@gmail.com>
2017-05-31 19:56:04 +0200

    Basic stuff inserted

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import praw
+import client_data
+
+reddit = praw.Reddit(
+    user_agent    = "GrammarNaziMachine2k 007 (by /u/SteveCCL",
+    client_id     = client_data.client_id,
+    client_secret = client_data.client_secret,
+    username      = client_data.username,
+    password      = client_data.password
+    )
