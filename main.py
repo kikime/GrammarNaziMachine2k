faaf721f1502a2c7c95ef1ce761310233aab9d8f
Niclas <njdm.evil@gmail.com>
2017-05-31 20:51:55 +0200

    Correct message

--- main.py
+++ main.py
@@ -38,9 +38,11 @@ try:
         if re.search(needs_fix_he_she, comment.body):
             comment.reply("You may use the *gender-neutral*, *singular* `they` when talking about a person with unknown gender. Click [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\nI've corrected {} people before you.\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.\n^If ^there's ^something ^wrong ^please ^message ^/u/SteveCCL.".format(corrected))
             corrected += 1
+            print("Corrected {}".format(comment))
         elif re.search(needs_fix_his_hers, comment.body):
             comment.reply("You may use the *gender-neutral*, *singular* `their` when talking about a person with unknown gender. Click [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\nI've corrected {} people before you.\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.\n^If ^there's ^something ^wrong ^please ^message ^/u/SteveCCL.".format(corrected))
             corrected += 1
+            print("Corrected {}".format(comment))
 except KeyboardInterrupt:
     print("User requested termination")
 
