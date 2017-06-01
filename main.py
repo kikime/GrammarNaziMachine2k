901deeb8f5bfc1c715c8d12b7ea6883036eddace
Niclas <njdm.evil@gmail.com>
2017-06-01 09:58:26 +0200

    Removed KeyboardInterrupt handler

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import praw
 
 print("GrammazNaziBot2k loading...")
 
-corrected = int(os.getenv('corrected', '0'))
-
-print("Already corrected {} people.".format(corrected))
 print("Bot loaded. Let's correct them!")
 
 needs_fix_he_she   = re.compile(r'\b(he\s*(/|\s+or\s+)\s*she|she\s*(/|\s+or\s+)\s*he)\b', re.IGNORECASE)
@@ -22,34 +19,34 @@ reddit = praw.Reddit(
     password      = os.environ['password']
 )
 
-try:
-    while 1:
-        try:
-            for comment in reddit.subreddit('all').stream.comments():
-                if comment.author.name == "GrammarNaziMachine2k":
-                    continue
-                if re.search(needs_fix_he_she, comment.body):
-                    comment.reply("You may use the *gender-neutral*, *singular* `they` instead of `he/she` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL.".format(corrected))
-                    corrected += 1
-                    print("Corrected #{}, {} ({})".format(corrected, comment.author.name, comment))
-                    os.environ['corrected'] = str(corrected)
-                elif re.search(needs_fix_his_hers, comment.body):
-                    comment.reply("You may use the *gender-neutral*, *singular* `their` instead of `his/hers` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL.".format(corrected))
-                    corrected += 1
-                    print("Corrected #{}, {} ({})".format(corrected, comment.author.name, comment))
-                    os.environ['corrected'] = str(corrected)
+def generate_reply(comment):
+    return None
 
-        except Exception as e:
-            if 'RATELIMIT: ' in str(e):
-                t = int(str(e).split(' ')[10])
-                print("RATELIMIT exceeded. Sleeping for {}....".format(t))
-                time.sleep(60 * t)
-            else:
-                print("Something went wrong ({})".format(type(e)))
-                print(e)
-                time.sleep(60)
-            print("Resuming")
-except KeyboardInterrupt:
-    print("User requested termination")
+while 1:
+    try:
+        for comment in reddit.subreddit('all').stream.comments():
+            if comment.author.name == "GrammarNaziMachine2k":
+                continue
+            """
+            if re.search(needs_fix_he_she, comment.body):
+                comment.reply("You may use the *gender-neutral*, *singular* `they` instead of `he/she` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL.".format(corrected))
+                corrected += 1
+                print("Corrected #{}, {} ({})".format(corrected, comment.author.name, comment))
+                os.environ['corrected'] = str(corrected)
+            elif re.search(needs_fix_his_hers, comment.body):
+                comment.reply("You may use the *gender-neutral*, *singular* `their` instead of `his/hers` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL.".format(corrected))
+                corrected += 1
+                print("Corrected #{}, {} ({})".format(corrected, comment.author.name, comment))
+                os.environ['corrected'] = str(corrected)
+            """
 
-print("See you soon")
+    except Exception as e:
+        if 'RATELIMIT: ' in str(e):
+            t = int(str(e).split(' ')[10])
+            print("RATELIMIT exceeded. Sleeping for {}....".format(t))
+            time.sleep(60 * t)
+        else:
+            print("Something went wrong ({})".format(type(e)))
+            print(e)
+            time.sleep(60)
+        print("Resuming")
