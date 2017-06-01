14ab1519df61aa5ef26eb6d675ddf133a8718925
Niclas <njdm.evil@gmail.com>
2017-06-01 10:05:18 +0200

    Tidied up

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ print("GrammazNaziBot2k loading...")
 
 print("Bot loaded. Let's correct them!")
 
-needs_fix_he_she   = re.compile(r'\b(he\s*(/|\s+or\s+)\s*she|she\s*(/|\s+or\s+)\s*he)\b', re.IGNORECASE)
-needs_fix_his_hers = re.compile(r'\b(his\s*(/|\s+or\s+)\s*hers|hers\s*(/|\s+or\s+)\s*his)\b', re.IGNORECASE)
-
 reddit = praw.Reddit(
     user_agent    = 'GrammarNaziMachine2k 007 (by /u/SteveCCL)',
     client_id     = os.environ['client_id'],
@@ -19,7 +16,18 @@ reddit = praw.Reddit(
     password      = os.environ['password']
 )
 
+needs_fix_he_she   = re.compile(r'\b(he\s*(/|\s+or\s+)\s*she|she\s*(/|\s+or\s+)\s*he)\b', re.IGNORECASE)
+
 def generate_reply(comment):
+    """Generates a reply base on the comment. Returns None if there shouldn't be a reply"""
+    reply = ""
+    if re.search(needs_fix_he_she, comment):
+        reply += "You may use the *gender-neutral*, *singular* `they` instead of `he/she` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info."
+
+    if len(reply) > 0:
+        reply += "\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL."
+        return reply
+
     return None
 
 while 1:
@@ -27,18 +35,10 @@ while 1:
         for comment in reddit.subreddit('all').stream.comments():
             if comment.author.name == "GrammarNaziMachine2k":
                 continue
-            """
-            if re.search(needs_fix_he_she, comment.body):
-                comment.reply("You may use the *gender-neutral*, *singular* `they` instead of `he/she` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL.".format(corrected))
-                corrected += 1
-                print("Corrected #{}, {} ({})".format(corrected, comment.author.name, comment))
-                os.environ['corrected'] = str(corrected)
-            elif re.search(needs_fix_his_hers, comment.body):
-                comment.reply("You may use the *gender-neutral*, *singular* `their` instead of `his/hers` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL.".format(corrected))
-                corrected += 1
-                print("Corrected #{}, {} ({})".format(corrected, comment.author.name, comment))
-                os.environ['corrected'] = str(corrected)
-            """
+
+            reply = generate_reply(comment.body)
+            if reply:
+                comment.reply(reply)
 
     except Exception as e:
         if 'RATELIMIT: ' in str(e):
