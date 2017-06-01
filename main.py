8d699002d37a95e412e32ee01dca5370b284a0f3
kikime <kikime@outlook.com>
2017-06-01 08:21:11 +0200

    Little more clearness to messages

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ try:
         try:
             for comment in reddit.subreddit('all').stream.comments():
                 if re.search(needs_fix_he_she, comment.body):
-                    comment.reply("You may use the *gender-neutral*, *singular* `they` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL.".format(corrected))
+                    comment.reply("You may use the *gender-neutral*, *singular* `they` instead of `he/she` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL.".format(corrected))
                     corrected += 1
                     print("Corrected #{}, {} ({})".format(corrected, comment.author.name, comment))
                     os.environ['corrected'] = str(corrected)
                 elif re.search(needs_fix_his_hers, comment.body):
-                    comment.reply("You may use the *gender-neutral*, *singular* `their` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL.".format(corrected))
+                    comment.reply("You may use the *gender-neutral*, *singular* `their` instead of `his/hers` when talking about a person with unknown gender.  \nClick [this](https://en.wikipedia.org/wiki/Singular_they) for more info.\n\n^Beep ^blop ^I'm ^a ^bot. ^I ^said ^beep ^blop ^I'm ^a ^bot.  \n^If ^there's ^something ^wrong ^please ^message ^SteveCCL.".format(corrected))
                     corrected += 1
                     print("Corrected #{}, {} ({})".format(corrected, comment.author.name, comment))
                     os.environ['corrected'] = str(corrected)
